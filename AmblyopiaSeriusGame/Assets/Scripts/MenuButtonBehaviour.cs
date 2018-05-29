124bf575905d26bdf6536ba3a08917d12fe4ca02
José Ricardo Zurita <jose.zurita@epn.edu.ec>
2018-05-28 19:10:23 -0500

    Esqueleto de menú principal, carga de clase de lógica de botones de menú

---
+++ AmblyopiaSeriusGame/Assets/Scripts/MenuButtonBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void LoadScene(int id)
+    {
+        SceneManager.LoadScene(id);
+    }
+
+    public void ExitButtonPressed()
+    {
+        Debug.Log("Salir de la aplicación");
+        Application.Quit();
+    }
+}
