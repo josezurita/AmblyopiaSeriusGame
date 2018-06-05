fa91cb4e7dbf6258eb593bfaa437ded8fdf5c5c7
David Davila <david.davila@epn.edu.ec>
2018-06-04 19:29:11 -0500

    Game 1: Añadido funciones basicas

---
+++ AmblyopiaSeriusGame/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public float jumpForce = 10f;
+    public Rigidbody2D rb;
+   
+
+    void Update () {
+		
+	}
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        Debug.Log(col.tag);
+        if(col.tag == "Circle")
+        {
+            if (!Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Lo lograste");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+        
+    }
+
+}
