eb2b7334b22200c65eee5c247d8a4873f75ccb17
David Davila <david.davila@epn.edu.ec>
2018-06-18 17:36:03 -0500

    Game1: Funcionalidad basica OK. Al hacer clic continua

--- AmblyopiaSeriusGame/Assets/Scripts/Player.cs
+++ AmblyopiaSeriusGame/Assets/Scripts/Player.cs
@@ -5,24 +5,48 @@ public class Player : MonoBehaviour {
 
     public float speed = 1f;
     public Rigidbody2D rb;
-   
+    public ColliderDistance2D colldist;
+    public GameObject circle1;
+    public Collider2D circle1coll;
+    public bool changedOverlapped =false ;
+    public bool wasOverlapped = false;
+    public bool clickedOn = false;
 
-    void Update () {
-        transform.Translate(speed*Time.deltaTime, 0, 0);
-	}
 
-    void OnTriggerEnter2D (Collider2D col)
+
+    private void Start()
     {
-        Debug.Log(col.tag);
-        if(col.tag == "Circle")
-        {
-            if (!Input.GetMouseButtonDown(0))
-            {
-                Debug.Log("Lo lograste");
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
-        
+        circle1coll = circle1.GetComponent<Collider2D>();
+
     }
+    void Update () {
+
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+        colldist = gameObject.GetComponent<Collider2D>().Distance(circle1coll);
+
+        changedOverlapped = (!colldist.isOverlapped && wasOverlapped) ? true : false ;
+        /*if (!colldist.isOverlapped && wasOverlapped)
+        {
+            changedOverlapped = true;
+        }
+        else
+        {
+            changedOverlapped = false;
+        }*/
+        if (colldist.isOverlapped && Input.GetMouseButtonDown(0))
+        {
+            clickedOn = true;
+            Debug.Log("clicked");
+        }
+
+        if (changedOverlapped && !clickedOn)
+        {
+            Debug.Log("GAME OVER!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        wasOverlapped = colldist.isOverlapped;
+    }
+
+    
 
 }
