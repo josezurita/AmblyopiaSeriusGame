0e734c65333f03e79caf1dcf2e17be1e1c6ea50a
David Davila <david.davila@epn.edu.ec>
2018-09-03 17:58:00 -0500

    Quitar escene del logo, añadir el cambio de escena en la primera carga

---
+++ AmblyopiaSeriusGame/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+
+    
+     // Use this for initialization
+    void Start () {
+
+        //Comentar linea de abajo para no resetear prefabs
+        PlayerPrefs.SetInt("isFirstTime", 0);
+        if (!PlayerPrefs.HasKey("isFirstTime") || PlayerPrefs.GetInt("isFirstTime") != 1)
+        {
+            // Show your prologue here.
+            //Salvar datos 
+        }
+        else
+        {
+            SceneManager.LoadScene("MainMenu");
+
+        }
+        // Now set the value of isFirstTime to be false in the PlayerPrefs.
+        PlayerPrefs.SetInt("isFirstTime", 1);
+        PlayerPrefs.Save();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+
+    }
+    
+        
+   
+
+}
---
+++ AmblyopiaSeriusGame/Assets/Scripts/SaveUserData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveUserData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // If there is no entry for isFirstTime means it is first time or if there is entry and it is not one means it is first time
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
